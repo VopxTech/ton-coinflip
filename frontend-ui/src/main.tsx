ae1c2c8f805c2ff415960b633c57257da4370e4a
Swafox <swafox@pm.me>
2024-04-09 21:32:37 +0300

    Fix manifest.json

--- frontend-ui/src/main.tsx
+++ frontend-ui/src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://raw.githubusercontent.com/VopxTech/ton-coinflip/main/frontend-ui/public/manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
