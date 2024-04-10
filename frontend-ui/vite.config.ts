308fe7ce0c00401c7b2f8a8439a6e3127fb6397e
Swafox <swafox@pm.me>
2024-04-10 19:05:01 +0300

    Configure builder to build to docs

--- frontend-ui/vite.config.ts
+++ frontend-ui/vite.config.ts
@@ -6,4 +6,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   base: "/frontend-ui/",
+  build: {
+    outDir: "../docs",
+  },
 });
