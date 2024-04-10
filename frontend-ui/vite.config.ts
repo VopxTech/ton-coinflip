3ee52904d214fb9a64e6fc1d54ce3ba48c1334bf
Swafox <swafox@pm.me>
2024-04-10 19:09:26 +0300

    Fix base in vite config

--- frontend-ui/vite.config.ts
+++ frontend-ui/vite.config.ts
@@ -5,7 +5,7 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  base: "/frontend-ui/",
+  base: "/ton-coinflip/",
   build: {
     outDir: "../docs",
   },
